f7e58d4358f2f0a53e7c7fa142e0add1c46d2b5e
luiz bueno <luiz504@hotmail.com>
2024-03-06 20:11:04 -0300

    feat: base notification integration and handling

--- App.tsx
+++ App.tsx
@@ -8,17 +8,23 @@ import {
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto'
 
-import { Routes } from './src/routes'
+import { CartContextProvider } from '~/contexts/CartContext'
 
-import { THEME } from './src/theme'
-import { Loading } from './src/components/Loading'
+import { THEME } from '~/theme'
 
-import { CartContextProvider } from './src/contexts/CartContext'
+import { Loading } from '~/components/Loading'
+import { Routes } from '~/routes'
+import { createTagUserInfo } from '~/notifications/notifications-tags'
+import { useEffect } from 'react'
 
 OneSignal.initialize(ANDROID_FIREBASE_CLIENT_ID)
 OneSignal.Notifications.requestPermission(true)
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
+  useEffect(() => {
+    createTagUserInfo()
+  }, [])
 
   return (
     <NativeBaseProvider theme={THEME}>
